c0d851fdc5ec54aeec72f439066190d045e86410
Elena Haran <elenaharan@gmail.com>
2024-02-05 18:19:06 +0000

    Flip conditions to move large amount of code from body of an if statement

--- src/Components/ChatRoom/ImagePreview.js
+++ src/Components/ChatRoom/ImagePreview.js
@@ -5,23 +5,21 @@ export default function ImagePreview(file) {
   const uploadStatus = useSimulateImgUpload();
   if (!file) return;
 
-  if (file.type.includes("image")) {
-    const separator = uploadStatus === "success" ? <hr /> : null;
+  if (!file.type.includes("image")) return null;
 
-    return (
-      <>
-        {separator}
-        <div className="p-3 mb-4">
-          <span className="text-light-grey">{file.name}</span>
-          <img
-            src={URL.createObjectURL(file)}
-            alt="File Preview"
-            className="w-16 h-16 object-cover rounded mr-2.5"
-          />
-        </div>
-      </>
-    );
-  }
+  const separator = uploadStatus === "success" ? <hr /> : null;
 
-  return null;
+  return (
+    <>
+      {separator}
+      <div className="p-3 mb-4">
+        <span className="text-light-grey">{file.name}</span>
+        <img
+          src={URL.createObjectURL(file)}
+          alt="File Preview"
+          className="w-16 h-16 object-cover rounded mr-2.5"
+        />
+      </div>
+    </>
+  );
 }
